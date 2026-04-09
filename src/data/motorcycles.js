9597934f6e2f2be747fd1d9e319e498e4afb9aea
FMatturroConnecteed <f.matturro@connecteed.com>
2026-04-09 15:01:10 +0200

    Aggiunta moto

--- src/data/motorcycles.js
+++ src/data/motorcycles.js
@@ -251,4 +251,452 @@ export const motorcycles = [
       "Adventure package"
     ]
   },
+  {
+    id: 10,
+    name: "Ducati Multistrada V4",
+    category: "Adventure",
+    price: "21.450",
+    image: "https://multistradav4.ducati.com/uploads/1771324336303.webp",
+    images: [
+      "https://multistradav4.ducati.com/uploads/1771324383966.webp",
+      "https://multistradav4.ducati.com/landings/117/images/gallery/Ducati-MY25-Multistrada-V4-overview-mosaic-gallery-780x430-04.webp",
+      "https://multistradav4.ducati.com/landings/117/images/gallery/Ducati-MY25-Multistrada-V4-overview-mosaic-gallery-442x600-06.webp"
+    ],
+    description: "La Multistrada V4 è la moto adventure più versatile e tecnologica di Ducati, perfetta per ogni tipo di viaggio.",
+    specs: {
+      engine: "1.158 cc V4 Granturismo",
+      power: "170 CV",
+      torque: "125 Nm",
+      weight: "215 kg",
+      topSpeed: "255 km/h"
+    },
+    features: [
+      "V4 Granturismo Engine",
+      "Cornering ABS",
+      "Adaptive Cruise Control",
+      "Blind Spot Detection",
+      "Skyhook Suspension EVO",
+      "5 Riding Modes"
+    ]
+  },
+  {
+    id: 11,
+    name: "BMW S 1000 RR",
+    category: "Sport",
+    price: "22.900",
+    image: "https://www.bmw-motorrad.it/content/dam/bmwmotorradnsc/common/multiimages/images/models/sport/s1000rr/2024/productstage/nsc-s1000rr-P0N3H-multiimage-1920x1200.jpg.asset.1732605093706.jpg",
+    images: [
+      "https://bmw.europe-moto.com/img/cms/s1000rrv1.jpg",
+      "https://images5.1000ps.net/b-f_W3012009-bmw-s-1000-rr-2025-gleiche-leistung-aber-umso-mehr-winglets-638653871257630580.jpg?format=webp&quality=80&scale=both&width=2816&height=1584&mode=crop",
+      "https://storage.edidomus.it/dueruote/nuovo/850/00054769.JPG"
+    ],
+    description: "La S 1000 RR è la superbike BMW che unisce prestazioni estreme con tecnologia all'avanguardia.",
+    specs: {
+      engine: "999 cc Inline-4",
+      power: "210 CV",
+      torque: "113 Nm",
+      weight: "197 kg",
+      topSpeed: "303 km/h"
+    },
+    features: [
+      "ShiftCam Technology",
+      "Dynamic Traction Control",
+      "Hill Start Control",
+      "ABS Pro",
+      "Dynamic ESA",
+      "Race Mode"
+    ]
+  },
+  {
+    id: 12,
+    name: "Kawasaki Z900",
+    category: "Naked",
+    price: "11.390",
+    image: "https://www.kawasaki.it/content/dam/products/pim/gallery/Resource_321266_26ZR900S_40TGN1ARF2CG_A.jpg/jcr:content/renditions/cq5dam.web.1280.1280.png",
+    images: [
+      "https://www.kawasaki.it/content/dam/products/pim/studio/Resource_320196_26ZR900S_40TGY1DRF3CG_A.jpg/_jcr_content/renditions/cq5dam.thumbnail.600.600.png",
+      "https://www.kawasaki.it/content/dam/products/pim/studio/Resource_320198_26ZR900S_40TGY1DRS3CG_A.jpg/_jcr_content/renditions/cq5dam.thumbnail.600.600.png",
+      "https://www.kawasaki.it/content/dam/products/pim/studio/Resource_320194_26ZR900S_40TGY1DLS3CG_A.jpg/_jcr_content/renditions/cq5dam.thumbnail.600.600.png"
+    ],
+    description: "La Z900 offre un mix perfetto tra prestazioni sportive e usabilità quotidiana.",
+    specs: {
+      engine: "948 cc Inline-4",
+      power: "125 CV",
+      torque: "98 Nm",
+      weight: "210 kg",
+      topSpeed: "240 km/h"
+    },
+    features: [
+      "KTRC 3-Mode Traction Control",
+      "KIBS ABS",
+      "Power Modes",
+      "Assist & Slipper Clutch",
+      "LED Lighting",
+      "Smartphone Connectivity"
+    ]
+  },
+  {
+    id: 13,
+    name: "Honda Africa Twin",
+    category: "Adventure",
+    price: "14.990",
+    image: "https://www.honda.it/content/dam/central/motorcycles/adventure/africa-twin-2024/Overview/tabbed-module/honda-africa-twin-tabbed-module-01-desktop.jpg/jcr:content/renditions/c4.jpg",
+    images: [
+      "https://www.honda.it/content/dam/central/motorcycles/adventure/africa-twin-2024/Overview/gallery/honda-africa-twin-gallery-1-desktop.jpg/jcr:content/renditions/c4_r.jpg",
+      "https://www.honda.it/content/dam/central/motorcycles/adventure/africa-twin-2024/Overview/gallery1/honda-africa-twin-gallery-desktop-3.jpg/jcr:content/renditions/c4_r.jpg",
+      "https://www.honda.it/content/dam/central/motorcycles/adventure/africa-twin-2026/gallery_5.jpg/jcr:content/renditions/c4_r.jpg"
+    ],
+    description: "L'Africa Twin è la leggendaria adventure bike Honda, nata per affrontare ogni terreno.",
+    specs: {
+      engine: "1.084 cc Parallel-Twin",
+      power: "102 CV",
+      torque: "105 Nm",
+      weight: "232 kg",
+      topSpeed: "190 km/h"
+    },
+    features: [
+      "G-Mode ABS",
+      "HSTC Traction Control",
+      "Throttle By Wire",
+      "5 Riding Modes",
+      "LED Lighting",
+      "Cruise Control"
+    ]
+  },
+  {
+    id: 14,
+    name: "Yamaha R1",
+    category: "Sport",
+    price: "19.990",
+    image: "https://imgs.search.brave.com/zJMul8iZMgLimJaXFXspGxUyCcdd2NefGkRzUrqiXNo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXQuY29t/L3cvZnVsbC9mLzQv/MS8xNzYyODEwLTI1/NjB4MTQ0MC1kZXNr/dG9wLWhkLXlhbWFo/YS15emYtcjEtd2Fs/bHBhcGVyLmpwZw",
+    images: [
+      "https://imgs.search.brave.com/Ro97eZIm4RX2eODTgez8l_6GNdjZkpxpBppx2VTLIHI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bW90b3Nob3B5YW1h/aGEuY29tL0hhbmRs/ZXJzL0ltYWdlLmFz/aHg_SUQ9OWVlMTNh/YzMtMmM4MC00MDQy/LWE1YjMtOWI3MWIx/NjQ2Mjc5JnNyYz0y/MDI0LXlhbWFoYS15/emYxMDAwcjEtZXUt/ZGV0YWlsLTAwNS0w/My5qcGcmd2g9QCUx/RFM",
+      "https://imgs.search.brave.com/vCqObs-BHEYYBP2VePSTswQ1AudAIBiJ7j2DtJqCxSM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXQuY29t/L3cvZnVsbC83L2Ev/Yy8xNzYyNTEzLTM4/NDB4MjE2MC1kZXNr/dG9wLTRrLXlhbWFo/YS15emYtcjEtd2Fs/bHBhcGVyLmpwZw",
+      "https://imgs.search.brave.com/HWfq4LOgGQOGOuCc1VUMRhLEJLD5jbcxYCFeGw9A6ys/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/bGlzdGluby5pbm1v/dG8uaXQvMjAyMy8x/Mi8xNC8yMDI0X1lB/TV9ZWkZfMTAwMF9S/MV9TUExfRVVfU01Y/X1NUQV8wMDFfMDNf/cHJldmlld19tYXhf/V2lkdGhfMTUwMF9t/YXhfSGVpZ2h0XzE1/MDBfcHBpXzMwMF9x/dWFsaXR5XzEwMF9h/MDk4YjU3ZDNiLmpw/Zw"
+    ],
+    description: "La R1 è la superbike Yamaha che porta la tecnologia MotoGP sulla strada.",
+    specs: {
+      engine: "998 cc Inline-4",
+      power: "200 CV",
+      torque: "112 Nm",
+      weight: "199 kg",
+      topSpeed: "299 km/h"
+    },
+    features: [
+      "Yamaha Ride Control",
+      "Brake Control System",
+      "Slide Control System",
+      "Lift Control System",
+      "Quick Shift System",
+      "6-Axis IMU"
+    ]
+  },
+  {
+    id: 15,
+    name: "Suzuki V-Strom 1050",
+    category: "Adventure",
+    price: "13.990",
+    image: "https://moto.suzuki.it/upl/cache/Suzuki_V-STROM_1050SE_04_V-Strom_1050-34304-800x470.jpg",
+    images: [
+      "https://moto.suzuki.it/upl/cache/Suzuki_V-STROM_1050SE_03_V-Strom_1050-768-800x470.jpg",
+      "https://moto.suzuki.it/upl/cache/Suzuki_V-STROM_1050SE_05_V-Strom_1050-50944-800x470.jpg",
+      "https://moto.suzuki.it/upl/cache/Suzuki_V-STROM_1050SE_V-STROM_1050_descrizione_1-65024-384x190.jpg"
+    ],
+    description: "La V-Strom 1050 è una adventure bike versatile e confortevole, perfetta per i lunghi viaggi.",
+    specs: {
+      engine: "1.037 cc V-Twin",
+      power: "107 CV",
+      torque: "100 Nm",
+      weight: "247 kg",
+      topSpeed: "200 km/h"
+    },
+    features: [
+      "Suzuki Intelligent Ride System",
+      "3-Mode Traction Control",
+      "Cruise Control",
+      "Easy Start System",
+      "Low RPM Assist",
+      "LED Lighting"
+    ]
+  },
+  {
+    id: 16,
+    name: "KTM 890 Duke R",
+    category: "Naked",
+    price: "12.490",
+    image: "https://azwecdnepstoragewebsiteuploads.azureedge.net/PHO_BIKE_DET_KTM-naked-bikes-990-duke-action-image4_%23SALL_%23AEPI_%23V1.jpg",
+    images: [
+      "https://azwecdnepstoragewebsiteuploads.azureedge.net/PHO_BIKE_90_RE_ktm-naked-bikes-990-duke-right-side-view-studio_%23SALL_%23AEPI_%23V1.png",
+      "https://azwecdnepstoragewebsiteuploads.azureedge.net/PHO_BIKE_PERS_LIHI_ktm-naked-bikes-990-duke-rear-left-view-studio_%23SALL_%23AEPI_%23V1.png",
+      "https://azwecdnepstoragewebsiteuploads.azureedge.net/PHO_BIKE_PERS_LIVO_ktm-naked-bikes-990-duke-front-left-view-studio_%23SALL_%23AEPI_%23V1.png"
+    ],
+    description: "La 890 Duke R è la naked sportiva che offre prestazioni eccezionali con un design aggressivo.",
+    specs: {
+      engine: "889 cc Parallel-Twin",
+      power: "121 CV",
+      torque: "99 Nm",
+      weight: "178 kg",
+      topSpeed: "245 km/h"
+    },
+    features: [
+      "Cornering ABS",
+      "Traction Control",
+      "Quickshifter+",
+      "WP APEX Suspension",
+      "Riding Modes",
+      "LED Lighting"
+    ]
+  },
+  {
+    id: 17,
+    name: "Triumph Street Triple 765 RS",
+    category: "Naked",
+    price: "12.950",
+    image: "https://media.triumphmotorcycles.co.uk/image/upload/f_auto/q_auto:eco/sitecoremedialibrary/media-library/images/motorcycles/roadsters-supersports/my23/street%20triple/765%20rs%20variant/media%20carousel/street-triple-765-rs-variant-media-slide-2-1920x1080.jpg",
+    images: [
+      "https://media.triumphmotorcycles.co.uk/image/upload/f_auto/q_auto:eco/sitecoremedialibrary/media-library/images/motorcycles/roadsters-supersports/my23/street%20triple/765%20rs%20variant/media%20carousel/street-triple-765-rs-variant-media-slide-3-1920x1080.jpg",
+      "https://media.triumphmotorcycles.co.uk/image/upload/f_auto/q_auto:eco/sitecoremedialibrary/media-library/images/motorcycles/roadsters-supersports/my23/street%20triple/765%20rs%20variant/media%20carousel/street-triple-765-rs-variant-media-slide-4-1920x1080.jpg",
+      "https://media.triumphmotorcycles.co.uk/image/upload/f_auto/q_auto:eco/sitecoremedialibrary/media-library/images/motorcycles/my25%20colours%20update/my25%20street%20triple%20765/my25-street-triple-765-rs-media-carousel1-1920x1080.jpg"
+    ],
+    description: "La Street Triple 765 RS è la naked sportiva che offre prestazioni da supersportiva con un motore tre cilindri eccezionale.",
+    specs: {
+      engine: "765 cc Inline-3",
+      power: "130 CV",
+      torque: "80 Nm",
+      weight: "189 kg",
+      topSpeed: "245 km/h"
+    },
+    features: [
+      "Cornering ABS",
+      "Traction Control",
+      "Quickshifter",
+      "Öhlins Suspension",
+      "Multiple Riding Modes",
+      "TFT Display"
+    ]
+  },
+  {
+    id: 18,
+    name: "Harley-Davidson Fat Boy 114",
+    category: "Cruiser",
+    price: "18.990",
+    image: "https://imgs.search.brave.com/rN7Jzzfowx48V_M5A1G6lrJAAzSm21-PWVz93Mako38/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cm9vbTU4LmNvbS8y/MDI1LzEyLzMxLzA3/ZGEzMmUwMTVlNTJj/YTFmYjY4MjdhMGEw/NThmZmQxXzRjNzM5/YmFjYjEyNjViNjIu/anBn",
+    images: [
+      "https://imgs.search.brave.com/7t0RVUQqc9k8SY9rSyWPSPJA23ulukeKe2Hunj6_R20/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdG9y/YWdlLmdvb2dsZWFw/aXMuY29tL21oaW1n/L3AvMzAxNi8xMjI4/MzAxNi9kMjk3X3Mu/anBn",
+      "https://imgs.search.brave.com/FXWfFxsAJvX_r4QzVWfbLZ7Eo7qrv7_5_dlx-jvLeDw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdG9y/YWdlLmdvb2dsZWFw/aXMuY29tL21oaW1n/L3AvMzAxNi8xMjI4/MzAxNi83YzY5X3Mu/anBn",
+      "https://imgs.search.brave.com/3TM6gCcHq0v6cK-CuRG6nOTgBYKHY3cFnLpNh1jJsGc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdG9y/YWdlLmdvb2dsZWFw/aXMuY29tL21oaW1n/L3AvNDQ3Ni8xMjMz/NDQ3Ni83ZTU0X3Mu/anBn"
+    ],
+    description: "La Fat Boy 114 è un'icona del design cruiser Harley-Davidson con un motore Milwaukee-Eight da 114.",
+    specs: {
+      engine: "1.868 cc Milwaukee-Eight 114",
+      power: "93 CV",
+      torque: "155 Nm",
+      weight: "305 kg",
+      topSpeed: "180 km/h"
+    },
+    features: [
+      "Reflex Defensive Rider Systems",
+      "ABS",
+      "Cruise Control",
+      "LED Lighting",
+      "Keyless Start",
+      "TFT Display"
+    ]
+  },
+  {
+    id: 19,
+    name: "MV Agusta F3 RR",
+    category: "Sport",
+    price: "24.990",
+    image: "https://www.mvagusta.com/media/beb48272-2ea7-429b-926c-1c7e745fcc9a/02.jpg",
+    images: [
+      "https://www.mvagusta.com/media/c6bb69a4-6bb1-475b-9f04-6542628f386a/03.jpg",
+      "https://www.mvagusta.com/media/00677657-34f9-48bb-9f4e-cecc01ac999c/04.jpg",
+      "https://www.mvagusta.com/media/16e17bdc-f7ce-44aa-b1de-cfb810841811/05.jpg"
+    ],
+    description: "La F3 RR è la supersportiva MV Agusta che unisce design italiano e prestazioni estreme.",
+    specs: {
+      engine: "798 cc Inline-3",
+      power: "147 CV",
+      torque: "88 Nm",
+      weight: "165 kg",
+      topSpeed: "265 km/h"
+    },
+    features: [
+      "MVRC 2.0 Traction Control",
+      "Cornering ABS",
+      "Quickshifter EAS 2.0",
+      "Launch Control",
+      "FCC 2.0 Slipper Clutch",
+      "5 Riding Modes"
+    ]
+  },
+  {
+    id: 20,
+    name: "Aprilia Tuono V4 Factory",
+    category: "Naked",
+    price: "19.990",
+    image: "https://wlassets.aprilia.com/wlassets/aprilia/master/Range/tuono_v4/family_page/2026/TuonoV4Factory---Hero---Desktop---1920x800/original/TuonoV4Factory+-+Hero+-+Desktop+-+1920x800.jpg?1773330301222",
+    images: [
+      "https://wlassets.aprilia.com/wlassets/aprilia/master/Range/tuono_v4/family_page/2026/WI-Aprilia-Tuono-V4-Factory-D-1920x1440-A/original/WI-Aprilia-Tuono+V4-Factory-D-1920x1440-A.jpg?1773244148842",
+      "https://wlassets.aprilia.com/wlassets/aprilia/master/Range/tuono_v4/family_page/2025/wall-image/Aprilia_TuonoV4_wall-image_1920x1280_3/original/Aprilia_TuonoV4_wall-image_1920x1280_3.jpg?1747323463779",
+      "https://wlassets.aprilia.com/wlassets/aprilia/master/Range/tuono_v4/family_page/2026/Tuono-V4-Gallery-2/original/Tuono+V4+Gallery+2.jpeg?1773330508861"
+    ],
+    description: "La Tuono V4 Factory è la naked sportiva che offre prestazioni da superbike con un manubrio più alto.",
+    specs: {
+      engine: "1.099 cc V4",
+      power: "175 CV",
+      torque: "121 Nm",
+      weight: "180 kg",
+      topSpeed: "285 km/h"
+    },
+    features: [
+      "APRC 2.0 System",
+      "Cornering ABS",
+      "Quickshifter",
+      "Launch Control",
+      "Wheelie Control",
+      "Traction Control"
+    ]
+  },
+  {
+    id: 21,
+    name: "Moto Guzzi V85 TT",
+    category: "Adventure",
+    price: "13.490",
+    image: "https://wlassets.motoguzzi.com/wlassets/moto-guzzi/master/Range/V85/PDP/V85_TT/wall_image/MG_V85TT_wall_image_4_1920x1440/original/MG_V85TT_wall_image_4_1920x1440.jpg?1710868784877",
+    images: [
+      "https://images.piaggio.com/motoguzzi/vehicles/gu6228300ebr00/gu6228300ebr01/gu6228300ebr01-01-m.png",
+      "https://wlassets.motoguzzi.com/wlassets/moto-guzzi/master/Range/V85/PDP/V85_TT/box/MG_Facelift_V85-TT-NuovaGamma_box_1920x1440_moto-intera_2/original/MG_Facelift_V85-TT-NuovaGamma_box_1920x1440_moto-intera_2.png?1710777303061",
+      "https://wlassets.motoguzzi.com/wlassets/moto-guzzi/master/Range/V85/PDP/V85_TT/gallery/MG_V85TT_wall_image_1920x1440_5/original/MG_V85TT_wall_image_1920x1440_5.jpg?1710925453234"
+    ],
+    description: "La V85 TT è l'adventure bike classica di Moto Guzzi con motore V-twin trasversale.",
+    specs: {
+      engine: "854 cc 90° V-twin",
+      power: "76 CV",
+      torque: "82 Nm",
+      weight: "230 kg",
+      topSpeed: "190 km/h"
+    },
+    features: [
+      "MGCT Traction Control",
+      "ABS Cornering",
+      "Cruise Control",
+      "Multiple Riding Modes",
+      "Full LED Lighting",
+      "TFT Display"
+    ]
+  },
+  {
+    id: 22,
+    name: "Benelli Leoncino 500",
+    category: "Naked",
+    price: "6.990",
+    image: "https://img.stcrm.it/images/20636845/HOR_STD/800x/benelli-leoncino-500-2020-07.jpg",
+    images: [
+      "https://cdn.keeway.com/benelli-3-0/media/7594/conversions/1-hd.png",
+      "https://cdn.keeway.com/benelli-3-0/media/7600/conversions/2-hd.png",
+      "https://cdn.keeway.com/benelli-3-0/media/7612/conversions/4-hd.png"
+    ],
+    description: "La Leoncino 500 è una naked di media cilindrata con stile retrò moderno.",
+    specs: {
+      engine: "500 cc Parallel-Twin",
+      power: "47 CV",
+      torque: "45 Nm",
+      weight: "192 kg",
+      topSpeed: "160 km/h"
+    },
+    features: [
+      "ABS",
+      "LED Lighting",
+      "LCD Display",
+      "Traction Control",
+      "Riding Modes",
+      "USB Port"
+    ]
+  },
+  {
+    id: 23,
+    name: "Husqvarna Norden 901",
+    category: "Adventure",
+    price: "13.990",
+    image: "https://www.planet-knox.com/wp-content/uploads/2021/11/2MC7396-1920x1280.jpg",
+    images: [
+      "https://www.planet-knox.com/wp-content/uploads/2021/11/2021-11-09-HUSQVARNA-NORDEN-G4-213-1500x1000.jpg",
+      "https://www.planet-knox.com/wp-content/uploads/2021/11/2021-11-09-HUSQVARNA-NORDEN-G4-626-1500x1000.jpg",
+      "https://www.planet-knox.com/wp-content/uploads/2021/11/98317_Norden-901-2022-1500x1000.jpg"
+    ],
+    description: "La Norden 901 è una adventure bike versatile con design scandinavo e prestazioni eccellenti.",
+    specs: {
+      engine: "889 cc Parallel-Twin",
+      power: "105 CV",
+      torque: "92 Nm",
+      weight: "204 kg",
+      topSpeed: "200 km/h"
+    },
+    features: [
+      "Cornering ABS",
+      "Traction Control",
+      "Quickshifter",
+      "WP Suspension",
+      "3 Riding Modes",
+      "TFT Display"
+    ]
+  },
+  {
+    id: 24,
+    name: "Royal Enfield Himalayan",
+    category: "Adventure",
+    price: "5.490",
+    image: "https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/himalayan/gallery/webp/thumbnail/gallery-1.webp",
+    images: [
+      "https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/himalayan/gallery/webp/thumbnail/gallery-5.webp",
+      "https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/himalayan/gallery/webp/thumbnail/gallery-2.webp",
+      "https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/himalayan/gallery/webp/thumbnail/gallery-6.webp"
+    ],
+    description: "L'Himalayan è la adventure bike accessibile di Royal Enfield, perfetta per ogni tipo di terreno.",
+    specs: {
+      engine: "411 cc Single",
+      power: "24 CV",
+      torque: "32 Nm",
+      weight: "185 kg",
+      topSpeed: "130 km/h"
+    },
+    features: [
+      "ABS",
+      "LED Lighting",
+      "Digital Display",
+      "USB Charger",
+      "Rack Mounts",
+      "Spoked Wheels"
+    ]
+  },
+  {
+    id: 25,
+    name: "Kawasaki Versys 1000",
+    category: "Adventure",
+    price: "14.990",
+    image: "https://imgs.search.brave.com/zCkZSYk13sd18lVWMjKuTMx6nykIhOltkCjjKOcQIAI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXM1LjEwMDBwcy5u/ZXQvYi1mX1czMDEx/OTcyLWthd2FzYWtp/LXZlcnN5cy0xMTAw/LXNlLXRlc3QtMjAy/NS02Mzg2NDc3MDc5/ODM4MDU0NTAuanBn/P2Zvcm1hdD13ZWJw/JnF1YWxpdHk9ODAm/dHJpbS50aHJlc2hv/bGQ9ODAmdHJpbS5w/ZXJjZW50cGFkZGlu/Zz0xJnNjYWxlPWJv/dGgmbW9kZT1jcm9w/JndpZHRoPTcwNiZo/ZWlnaHQ9NDcyJmJn/Y29sb3I9cmdiYV8z/OV80Ml80NF8w",
+    images: [
+      "https://www.kawasaki.it/content/dam/products/pim/studio/ver/Resource_323107_26MY_Versys_1100_SE_BU1_TDF.jpg/_jcr_content/renditions/cq5dam.thumbnail.600.600.png",
+      "https://www.kawasaki.it/content/dam/products/pim/studio/Resource_320364_26KLZ1100C_40TBU1DRS3CG_A.jpg/_jcr_content/renditions/cq5dam.thumbnail.600.600.png",
+      "https://www.kawasaki.it/content/dam/products/pim/studio/Resource_320357_26KLZ1100C_40TBU1DLS3CG_A.jpg/_jcr_content/renditions/cq5dam.thumbnail.600.600.png"
+    ],
+    description: "La Versys 1000 è una adventure bike sportiva e versatile, perfetta per i lunghi viaggi.",
+    specs: {
+      engine: "1.043 cc Inline-4",
+      power: "120 CV",
+      torque: "102 Nm",
+      weight: "250 kg",
+      topSpeed: "210 km/h"
+    },
+    features: [
+      "KTRC Traction Control",
+      "KIBS ABS",
+      "Power Modes",
+      "Electronic Throttle Valves",
+      "LED Lighting",
+      "Cruise Control"
+    ]
+  }
 ];
